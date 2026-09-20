aceaa009009c947824f83c3290c879c60d20a462
GERRY Express <krittameth2020@gmail.com>
2026-09-20 20:52:12 +0700

    feat: add BaseInput, BaseButton, BaseToggle components

---
+++ frontend/src/components/BaseButton.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+interface BaseButtonProp {
+    text: string
+}
+withDefaults(defineProps<BaseButtonProp>(), {
+    text: ''
+}
+)
+</script>
+<template>
+    <button class="bg-action hover:bg-lime-400 transition-all w-full px-5 py-3 rounded-xl shadow-md hover:shadow-xl cursor-pointer text-tertiary font-bold" v-bind="$attrs">
+        {{ text }}
+    </button>
+</template>
---
+++ frontend/src/components/BaseInput.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+const modelValue = defineModel()
+interface BaseInputProps {
+    label?: string,
+    placeholder?: string
+}
+withDefaults(defineProps<BaseInputProps>(), {
+    label: '',
+    placeholder: ''
+})
+</script>
+
+<template>
+    <label :for="label" class="w-full font-bold">{{ label }}</label>
+    <input
+        class="w-full bg-tertiary rounded-lg text-white p-2"
+        :id="label"
+        v-model="modelValue"
+        v-bind="$attrs"
+        :placeholder="placeholder"
+        type="text"/>
+</template>
