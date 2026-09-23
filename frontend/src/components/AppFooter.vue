6b164819bd0288b9a4da09511f4e10edcfb0ab26
GERRY Express <krittameth2020@gmail.com>
2026-09-23 09:00:39 +0700

    feat: add axios, NavBar, Footer

---
+++ frontend/src/components/AppFooter.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { Copyright, InfoIcon } from '@lucide/vue';
+
+</script>
+
+
+<template>
+    <footer class="flex flex-row justify-between items-center px-12 py-16 h-96 w-full bg-tertiary">
+        <div class="flex gap-x-1.5">
+            <Copyright/><p>2027 KaiDao-5-Fong</p>
+        </div>
+        <RouterLink class="flex gap-x-1.5" :to="{ name: '' }">
+            <InfoIcon/>CONTACT US
+        </RouterLink>
+    </footer>
+</template>
---
+++ frontend/src/components/AppNavItem.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { Heart, ShoppingCart, UserRound } from '@lucide/vue';
+interface NavItemProps {
+    name: string
+    routeName: string
+}
+withDefaults(defineProps<NavItemProps>(), {
+    name: '',
+    routeName: ''
+})
+</script>
+
+
+<template>
+    <RouterLink :to="{ name: 'landing-page-view' }" class="flex items-center justify-center bg-secondary text-white! rounded-full size-12 shadow-md">
+        <ShoppingCart v-if="name === 'cart'"/>
+        <Heart v-else-if="name === 'wishlist'"/>
+        <UserRound v-else-if="name === 'profile'"/>
+    </RouterLink>
+</template>
