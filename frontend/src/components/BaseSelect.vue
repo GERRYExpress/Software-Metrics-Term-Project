2bccdd7415bdf351d75f9b33e8b50b7b69f402f8
GERRY Express <krittameth2020@gmail.com>
2026-09-23 12:05:28 +0700

    refactor(BaseSelect): change label into a normal text, add id

--- frontend/src/components/BaseSelect.vue
+++ frontend/src/components/BaseSelect.vue
@@ -1,6 +1,8 @@
 <script setup lang="ts">
 import type { Category } from '@/types';
 import { ChevronsUpDown } from '@lucide/vue';
+import { useId } from 'vue';
+const inputId = useId()
 
 const modelValue = defineModel()
 interface OptionProps {
@@ -11,9 +13,9 @@ defineProps<OptionProps>()
 </script>
 
 <template>
-    <label v-if="label">{{ label }}</label>
+    {{ label }}
     <div class="flex flex-row">
-        <select 
+        <select :id="inputId"
             class="bg-white text-tertiary! transition-all hover:scale-101 h-full pl-2.5 pr-6 text-lg align-middle appearance-none border border-gray-400 focus:border-secondary focus:outline-none"
             v-bind="$attrs" v-if="options"
             v-model="modelValue">
