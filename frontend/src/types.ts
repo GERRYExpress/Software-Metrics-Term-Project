49a71f1465db5f5e1a2efe82ee579093fc95e0ff
GERRY Express <krittameth2020@gmail.com>
2026-09-23 09:04:47 +0700

    feat: add Account, Game, Category types

--- frontend/src/types.ts
+++ frontend/src/types.ts
@@ -9,3 +9,32 @@ export interface RegisterForm {
     password: string
     confirmPassword: string
 }
+
+export interface Account {
+    id: number
+    username: string
+    password: string
+    createdDate: Date
+    isBuyer: boolean
+    isSeller: boolean
+    totalGame: number
+    totalSpend: number
+    totalSale: number
+}
+
+export interface Game {
+    id: number
+    releaseDate: Date
+    categories: Category[]
+    name: string
+    description: string
+    price: number
+    posterUrl: string
+    account: Account
+}
+
+export interface Category {
+    id: number
+    name: string
+    isHidden: boolean
+}
