cf26936c3bb4ae7510988c78048ec03b8689ea4c
GERRY Express <krittameth2020@gmail.com>
2026-09-23 09:01:01 +0700

    feat: add GameCard, BuyerLayoutView

---
+++ frontend/src/components/GameCard.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import { Heart, ShoppingCartPlus } from '@lucide/vue';
+import BaseButton from './BaseButton.vue';
+import type { Game } from '@/types.ts';
+withDefaults(defineProps<{
+    game?: Game | null
+}>(), ({
+    game: null
+}))
+</script>
+
+
+<template>
+    <RouterLink :to="{ name: '' }" class="w-auto w-max-72 rounded-xl bg-tertiary flex flex-col relative">
+        <img class="w-full h-2/3 rounded-t-xl pointer-events-none" src="https://picsum.photos/128" alt="">
+        <div class="bg-tertiary w-24 px-2 text-center truncate line-clamp-1 absolute place-self-end font-normal rounded-bl-xl rounded-tr-xl">
+            {{ game?.categories.at(0) ?? 'CATEGORY' }}
+        </div>
+        <div class="p-2 w-full h-full flex flex-col gap-y-2">
+            <p>{{ game?.name ?? 'NAME' }}</p>
+            <p class="text-end text-xl font-bold">${{ game?.price ?? 'PRICE' }}</p>
+            <div class="flex flex-row gap-x-1">
+                <BaseButton>
+                    <Heart/>
+                </BaseButton>
+                <BaseButton>
+                    <ShoppingCartPlus/>
+                </BaseButton>
+            </div>
+        </div>
+    </RouterLink>
+</template>
---
+++ frontend/src/views/BuyerLayoutView.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import AppFooter from '@/components/AppFooter.vue';
+import AppNavbar from '@/components/AppNavbar.vue';
+
+</script>
+
+<template>
+    <div class="flex flex-col w-full h-screen">
+        <AppNavbar/>
+        <RouterView/>
+        <AppFooter/>
+    </div>
+</template>
