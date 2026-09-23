c21e044bc9e6c610325486f6260b45b29eb05c34
GERRY Express <krittameth2020@gmail.com>
2026-09-23 13:44:41 +0700

    feat: add contactview

---
+++ frontend/src/views/ContactView.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import AppCard from '@/components/AppCard.vue';
+
+</script>
+
+
+<template>
+    <h1>Contacts</h1>
+    <AppCard>
+        <p>Address: College of Arts, Media, and Technology, Chiang Mai University</p>
+    </AppCard>
+</template>
