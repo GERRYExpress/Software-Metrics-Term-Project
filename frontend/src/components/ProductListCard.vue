9525f65fc5a33dde0cae4ba94cef538eccb74d14
GERRY Express <krittameth2020@gmail.com>
2026-09-24 19:39:10 +0700

    feat: add PriceSummary Card, ProductListCard, and finish CartView

---
+++ frontend/src/components/ProductListCard.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import AppCard from './AppCard.vue';
+import BaseButton from './BaseButton.vue';
+
+</script>
+
+
+
+<template>
+    <AppCard class="grid grid-cols-3 place-items-start! shadow-md hover:shadow-xl transition-all hover:scale-101 relative">
+        <img class="w-24 rounded-xl col-span-1" src="https://picsum.photos/128/192" alt="">
+        <div class="grid grid-cols-2 col-span-2 w-full">
+            <div class=" w-full text-start">
+                <p class="text-md font-bold">GAME 1</p>
+                <p class="text-sm">CATEGORY</p>
+                <p class="text-md font-thin ">Added:</p>
+            </div>
+            <div class="w-full text-end">
+                <p>$PRICE</p>
+            </div>
+        </div>
+        <div class="absolute bottom-0 right-0 w-24!">
+            <BaseButton type="danger-link">Remove</BaseButton>
+        </div>
+    </AppCard>
+
+</template>
--- frontend/src/views/CartView.vue
+++ frontend/src/views/CartView.vue
@@ -1,3 +1,18 @@
+<script setup lang="ts">
+import PriceSummaryCard from '@/components/PriceSummaryCard.vue';
+import ProductListCard from '@/components/ProductListCard.vue';
+import ProductMainContainer from '@/components/ProductMainContainer.vue';
+
+</script>
+
 <template>
-    <h1 class="full">Cart</h1>
+    <ProductMainContainer class="w-2xl place-self-center">
+        <h1 class="w-2xl">Cart</h1>
+        <div class="grid grid-cols-4 w-full gap-x-3">
+            <div class="w-full col-span-3 flex flex-col gap-y-2.5">
+                <ProductListCard v-for="value in 5" :key="value"/>
+            </div>
+            <PriceSummaryCard/>
+        </div>
+    </ProductMainContainer>
 </template>
