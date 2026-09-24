24ab2db7e3f371e4a92caa6cf7297c385281e6f9
GERRY Express <krittameth2020@gmail.com>
2026-09-24 19:39:10 +0700

    feat: add PriceSummary Card, ProductListCard, and finish CartView

---
+++ frontend/src/components/PriceSummaryCard.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import type { Game } from '@/types.ts';
+import AppCard from './AppCard.vue';
+import BaseButton from './BaseButton.vue';
+defineProps<{
+    games?: Game[]
+}>()
+
+</script>
+
+
+<template>
+    <AppCard :class="{ 'bg-action': !games}" class="items-start justify-start h-min max-h-96">
+        <div v-for="game in games" :key="game.id" class="flex flex-row justify-between w-full">
+            <p>{{ game.name }}</p>
+            <p>${{ game.price }}</p>
+        </div>
+        <div class="flex flex-row justify-between w-full font-bold">
+            <p>Total</p>
+            <p>$0.00</p>
+        </div>
+        <BaseButton>Checkout</BaseButton>
+    </AppCard>
+</template>
