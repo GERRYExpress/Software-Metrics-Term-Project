5923fa63e51bb5200d1d5d97cb5ffc5207d7167a
GERRY Express <krittameth2020@gmail.com>
2026-09-24 19:39:26 +0700

    feat: add WishlistView

--- frontend/src/views/WishlistView.vue
+++ frontend/src/views/WishlistView.vue
@@ -1,4 +1,15 @@
+<script setup lang="ts">
+import ProductListCard from '@/components/ProductListCard.vue';
+import ProductMainContainer from '@/components/ProductMainContainer.vue';
+
+</script>
+
 
 <template>
-    <h1>Wishlists</h1>
+    <ProductMainContainer class="w-2xl place-self-center">
+        <h1 class="w-2xl">Wishlists</h1>
+        <div class="w-full col-span-3 flex flex-col gap-y-2.5">
+            <ProductListCard v-for="value in 5" :key="value"/>
+        </div>
+    </ProductMainContainer>
 </template>
