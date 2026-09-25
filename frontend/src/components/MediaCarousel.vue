6e4b7689e9ffaf8cc4d871a0f5c7cade44503988
GERRY Express <krittameth2020@gmail.com>
2026-09-25 09:13:28 +0700

    feat: add MediaCarousel

---
+++ frontend/src/components/MediaCarousel.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import type { Media } from '@/types';
+import { computed } from 'vue';
+const props = defineProps<{
+    media: Media[] | null,
+    posterUrl: string
+}>()
+
+const posterUrl = computed(() => {
+    return props.posterUrl === '' ? 'https://placehold.co/128x192?text=Poster+Unavailable' : props.posterUrl
+})
+</script>
+
+<template>
+    <img class="rounded-xl" :src="posterUrl"/>
+</template>
