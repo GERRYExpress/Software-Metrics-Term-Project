a2e9a5f85edd327a90fe98169fceb288f0fdbe56
GERRY Express <krittameth2020@gmail.com>
2026-09-25 20:31:16 +0700

    style(ProductDetailView): restructure IA

--- frontend/src/views/ProductDetailView.vue
+++ frontend/src/views/ProductDetailView.vue
@@ -6,32 +6,50 @@ import ProductMainContainer from '@/components/ProductMainContainer.vue';
 import SystemRequirement from '@/components/SystemRequirement.vue';
 import { useGameStore } from '@/stores/game';
 import { Heart } from '@lucide/vue';
+import { Image } from '@unpic/vue';
 import { storeToRefs } from 'pinia';
+import { computed } from 'vue';
 const gameStore = useGameStore()
 const { name, description, releaseDate, categories, price, media, hardwareRequirement, posterUrl, account } = storeToRefs(gameStore)
+
+const formattedCategories = computed(() => {
+    if (categories.value) {
+        return categories.value.map(category => category.name)
+    }
+})
+const formattedDate = computed(() => {
+    if (releaseDate.value) {
+        const date = new Date(releaseDate.value)
+        return date.getDate() + '/' + date.getMonth() + '/' + date.getFullYear()
+    }
+})
 </script>
 
 
 
 <template>
     <ProductMainContainer>
-        <AppCard class="grid grid-cols-2">
-            <MediaCarousel :media :posterUrl/>
-            <div class="flex flex-col">
+        <AppCard class="grid grid-cols-2 gap-x-5">
+            <Image class="rounded-xl w-full" layout="constrained" :src="posterUrl"/>
+            <div class="flex flex-col place-self-start h-full gap-y-2">
                 <h2>{{ name }}</h2>
-                <h3>Published on <b>{{ releaseDate ?? 'XX/XX/XXXX' }}</b> by <b>{{ account?.username ?? 'USER' }}</b></h3>
-                <h3>{{ categories }}</h3>
-                <h2>${{ price.toFixed(2) }}</h2>
+                <h3>Published on <b>{{ formattedDate ?? 'XXXXXXX' }}</b> by <b>{{ account?.username ?? 'USER' }}</b></h3>
+                <h3><span v-for="category in formattedCategories" :key="category">{{ category + ' ' }}</span></h3>
+                <h2 class="mt-auto text-end">${{ price.toFixed(2) }}</h2>
                 <div class="flex gap-x-2.5">
                     <BaseButton class="w-2/3">Add to Cart</BaseButton>
                     <BaseButton type="neutral" class="w-1/3!"><Heart/></BaseButton>
                 </div>
             </div>
         </AppCard>
+        <h2 class="w-2xl">Gallery</h2>
+        <AppCard class="text-justify">
+            <!-- CAROUSEL MODIFIACTION NEEDED --Krittameth to Boki Chan -->
+            <MediaCarousel :media/>
+        </AppCard>
         <h2 class="w-2xl">Description</h2>
         <AppCard class="text-justify">
             {{ description }}
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quisquam quas unde adipisci beatae placeat dolore at libero nisi hic doloribus magnam omnis ab, aliquid, corrupti voluptatum nostrum ut? Laudantium, quisquam.
         </AppCard>
         <h2 class="w-2xl">System Requirements</h2>
         <AppCard class="grid grid-cols-2 gap-5">
