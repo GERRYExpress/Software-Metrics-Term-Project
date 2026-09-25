5d58e7190a4696e71e6d7554fb1dc0d5fdffc32b
GERRY Express <krittameth2020@gmail.com>
2026-09-25 08:54:50 +0700

    style(WishlistView): fix product card length is not full

--- frontend/src/views/WishlistView.vue
+++ frontend/src/views/WishlistView.vue
@@ -8,7 +8,7 @@ import ProductMainContainer from '@/components/ProductMainContainer.vue';
 <template>
     <ProductMainContainer class="w-2xl place-self-center">
         <h1 class="w-2xl">Wishlists</h1>
-        <div class="w-full col-span-3 flex flex-col gap-y-2.5">
+        <div class="w-full *:w-full col-span-3 flex flex-col gap-y-2.5">
             <ProductListCard v-for="value in 5" :key="value"/>
         </div>
     </ProductMainContainer>
