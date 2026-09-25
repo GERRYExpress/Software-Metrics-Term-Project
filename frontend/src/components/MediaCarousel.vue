25b5acc28590e36964e2eefd1f2456f869946ee1
GERRY Express <krittameth2020@gmail.com>
2026-09-25 20:30:44 +0700

    chore: init MediaCarousel

--- frontend/src/components/MediaCarousel.vue
+++ frontend/src/components/MediaCarousel.vue
@@ -1,16 +1,10 @@
 <script setup lang="ts">
 import type { Media } from '@/types';
-import { computed } from 'vue';
-const props = defineProps<{
+defineProps<{
     media: Media[] | null,
-    posterUrl: string
 }>()
-
-const posterUrl = computed(() => {
-    return props.posterUrl === '' ? 'https://placehold.co/128x192?text=Poster+Unavailable' : props.posterUrl
-})
 </script>
 
 <template>
-    <img class="rounded-xl" :src="posterUrl"/>
+    <div></div>
 </template>
